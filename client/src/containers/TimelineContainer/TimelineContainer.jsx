a0cb2c550c294fbeb2680d8e1c800cb12d8c4640
TakuyaTaniguchi <ttaniguchi@binc.jp>
2021-12-28 12:43:28 +0900

    test

--- client/src/containers/TimelineContainer/TimelineContainer.jsx
+++ client/src/containers/TimelineContainer/TimelineContainer.jsx
@@ -6,7 +6,7 @@ import { TimelinePage } from '../../components/timeline/TimelinePage';
 import { useInfiniteFetch } from '../../hooks/use_infinite_fetch';
 import { fetchJSON } from '../../utils/fetchers';
 
-/** @type {React.VFC} */
+/** @type {React.VFC} test*/
 const TimelineContainer = () => {
   const { data: posts, fetchMore } = useInfiniteFetch('/api/v1/posts', fetchJSON);
 
